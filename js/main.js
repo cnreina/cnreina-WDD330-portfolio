8ca030882ad280f963bb85c0965eae9184af3fa3
Carlos N Reina <cnreina@gmail.com>
2022-09-13 18:12:18 -0600

    ...

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
-const backdrop    = document.querySelector('.backdrop');
-const sideDrawer  = document.querySelector('.mobile-nav');
-const menuToggle  = document.querySelector('#side-menu-toggle');
+/*	Carlos N Reina
+  cnreina@gmail.com
+*/
 
-function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
-}
 
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
-}
+const links = [
+  {
+    label: "Week 1",
+    url: "html/w01/w01.html"
+  },
+  {
+    label: "Week 2",
+    url: "html/w02/w02.html"
+  }
+];
 
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
+window.onload = function() {
+  console.log("window.onload");
+  loadMainLinks();
+};
+
+function loadMainLinks() {
+	console.log("loadLinks");
+	// load links
+  const linksElement = document.getElementById("links-list");
+  var linksCounter = 0;
+  var linksTotalCount = links.length;
+  for (linksCounter = 0; linksCounter < linksTotalCount; linksCounter++) {
+    // create new list item
+    var liElement = document.createElement('li');
+    // insert url
+    var aElement = document.createElement('a');
+    aElement.setAttribute("href", links[linksCounter].url);
+    liElement.appendChild(aElement);
+    // insert label
+    var h3Element = document.createElement('h3');
+    aElement.appendChild(h3Element);
+    h3Element.appendChild(document.createTextNode(links[linksCounter].label));
+    // add item to list
+    linksElement.appendChild(liElement);
+  }
+
+};
