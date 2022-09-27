83b1943ea3a2ce9fb3b2b528e7d8e8670a2ca742
Carlos N Reina <cnreina@gmail.com>
2022-09-26 18:40:54 -0600

    ...

--- js/w03.js
+++ js/w03.js
@@ -2,12 +2,6 @@
   cnreina@gmail.com
 */
 
-// const quiz = [
-//   ["What is Superman's real name?","Clark Kent"],
-//   ["What is Wonder Woman's real name?","Diana Prince"],
-//   ["What is Batman's real name?","Bruce Wayne"]
-// ];
-
 const quiz = [
   { name: "Superman",realName: "Clark Kent" },
   { name: "Wonder Woman",realName: "Diana Prince" },
