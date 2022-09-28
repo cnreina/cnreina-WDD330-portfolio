9fc3bc1e7122b8a56a9bdd47ac7f78f2bea6cd1c
Carlos N Reina <cnreina@gmail.com>
2022-09-27 18:06:17 -0600

    ...

--- js/w0302.js
+++ js/w0302.js
@@ -3,11 +3,6 @@
   cnreina.com
 */
 
-// Get your shorts on - this is an array workout!
-// ## Array Cardio Day 1
-
-// Some data we can work with
-
 const inventors = [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -23,88 +18,7 @@ const inventors = [
   { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ];
 
-const people = [
-  'Beck, Glenn',
-  'Becker, Carl',
-  'Beckett, Samuel',
-  'Beddoes, Mick',
-  'Beecher, Henry',
-  'Beethoven, Ludwig',
-  'Begin, Menachem',
-  'Belloc, Hilaire',
-  'Bellow, Saul',
-  'Benchley, Robert',
-  'Benenson, Peter',
-  'Ben-Gurion, David',
-  'Benjamin, Walter',
-  'Benn, Tony',
-  'Bennington, Chester',
-  'Benson, Leana',
-  'Bent, Silas',
-  'Bentsen, Lloyd',
-  'Berger, Ric',
-  'Bergman, Ingmar',
-  'Berio, Luciano',
-  'Berle, Milton',
-  'Berlin, Irving',
-  'Berne, Eric',
-  'Bernhard, Sandra',
-  'Berra, Yogi',
-  'Berry, Halle',
-  'Berry, Wendell',
-  'Bethea, Erin',
-  'Bevan, Aneurin',
-  'Bevel, Ken',
-  'Biden, Joseph',
-  'Bierce, Ambrose',
-  'Biko, Steve',
-  'Billings, Josh',
-  'Biondo, Frank',
-  'Birrell, Augustine',
-  'Black, Elk',
-  'Blair, Robert',
-  'Blair, Tony',
-  'Blake, William'
-];
-
-// Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
+/* ************************************************************************* */
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
-
-// Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
-
-// Array.prototype.reduce()
-// 4. How many years did all the inventors live?
-
-// 5. Sort the inventors by years lived
-
-// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-// goto the link above and open the console. Paste the following two lines in.  That will create a list of links in memory that you can reference through the console. Use that list to finish the problem.
-// const category = document.querySelector('.mw-category');
-// const links = Array.from(category.querySelectorAll('a'));
-
-// 7. sort Exercise
-// Sort the people alphabetically by last name
-
-// 8. Reduce Exercise
-// Sum up the instances of each of these
-const data = [
-  'car',
-  'car',
-  'truck',
-  'truck',
-  'bike',
-  'walk',
-  'car',
-  'van',
-  'bike',
-  'walk',
-  'car',
-  'van',
-  'car',
-  'truck'
-];
--- js/w0304.js
+++ js/w0304.js
@@ -20,11 +20,5 @@ const inventors = [
 
 /* ************************************************************************* */
 
-// Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
-
-document.getElementById("textarea1").innerHTML = inventors.filter(checkCentury);
-
-function checkCentury(inventors) {
-  return inventors.year >= 1500 && inventors.year <= 1599;
-};
+// Array.prototype.reduce()
+// 4. How many years did all the inventors live?
