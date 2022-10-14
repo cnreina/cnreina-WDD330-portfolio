4bca72b86787bb7f37a2cadf4a5160de5e9cbdd4
Carlos N Reina <cnreina@gmail.com>
2022-10-13 18:23:04 -0600

    ...

--- js/w05/w0501B.js
+++ js/w05/w0501B.js
@@ -10,12 +10,19 @@
 window.onload = function () {
   // init event listeners
   const cnrDetailsLinksVar = document.querySelectorAll('.backlinks');
-  for(const cnrDetailsLinkVar of cnrDetailsLinksVar) {
-    cnrDetailsLinkVar.addEventListener('click', cnrBackLinksClickHandler);
+  for (const cnrDetailsLinkVar of cnrDetailsLinksVar) {
+    const gridContainer = document.getElementById('gridcontainer');
+    if ("ontouchend" in document.documentElement) {
+      console.log("Using touchend");
+      cnrDetailsLinkVar.addEventListener('touchend', cnrBackLinksClickHandler);
+    }
+    else {
+      console.log("Using click");
+      cnrDetailsLinkVar.addEventListener('click', cnrBackLinksClickHandler);
+    };
   };
   
   cnrWindowOnLoadHandler();
-
 };
 
 // EVENT HANDLERS
