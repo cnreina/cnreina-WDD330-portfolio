28803dfde01a1382d1b9f7247981cdd04fc8390f
Carlos N Reina <cnreina@gmail.com>
2022-10-18 17:30:16 -0600

    ...

--- js/w06/cnrDataModule.js
+++ js/w06/cnrDataModule.js
@@ -6,90 +6,184 @@
 
 /* ************************************************************************* */
 
+export class cnrDataItemClass {
+  // PRIVATE
 
-export class cnrDataListClass {
-  constructor() {
-    this.cnrDataList = new [{}];
-    cnrDataList.add
+  #cnrItemID;
+  #cnrItemCreatedTimeUTC;
+  #cnrItemType;
+  #cnrItemTag;
+  #cnrItemData;
+
+  constructor(cnrTypeParam, cnrTagParam, cnrDataParam) {
+    this.#cnrItemID = self.crypto.randomUUID();
+    const cnrDateTimeVar = new Date();
+    const cnrDateTimeUTCVar = cnrDateTimeVar.toUTCString();
+    this.#cnrItemCreatedTimeUTC = cnrDateTimeUTCVar;
+    this.#cnrItemType = cnrTypeParam;
+    this.#cnrItemTag = cnrTagParam;
+    this.#cnrItemData = cnrDataParam;
   };
 
-  cnrDataGetList() { return this.cnrDataList; };
+  // PUBLIC
 
-  cnrDataCreateItem() {
-    cnrNewItemVar = new cnrDataItemClass();
+  /**	Returns item object. */
+  cnrDataItemGetItem() { return this; };
+
+  /**	Returns item's UUID. */
+  cnrDataItemGetID() { return this.#cnrItemID; };
+
+  /**	Returns item's date and time of creation in UTC format. */
+  cnrDataItemGetCreatedTime() { return this.#cnrItemCreatedTimeUTC; };
+
+  /**	Returns item's data type (Ex; string, json). */
+  cnrDataItemGetType() { return this.#cnrItemType; };
+
+  /**	Returns item's key. */
+  cnrDataItemGetTag() { return this.#cnrItemTag; };
+
+  /**	Returns item's data. */
+  cnrDataItemGetData() { return this.#cnrItemData; };
+
+  /**	Sets item's data type (Ex; string, json). 
+   * Returns if passed type is null or empty.
+   */
+   cnrDataItemUpdateType(cnrTypeParam) {
+    if (cnrTypeParam == null || cnrTypeParam == '') { return; };
+
+    this.#cnrItemType = cnrTypeParam;
+  };
+
+  /**	Sets item's key. 
+   * Returns if passed key is null or empty.
+   * Key is not unique
+   */
+  cnrDataItemUpdateTag(cnrTagParam) {
+    if (cnrTagParam == null || cnrTagParam == '') { return; };
+
+    this.#cnrItemTag = cnrTagParam;
+  };
+
+  /**	Sets item's value. 
+   * Sets it to empty string if passed value is null, empty, or not a string.
+   */
+  cnrDataItemUpdateData(cnrDataParam) {
+    if (cnrDataParam == null || cnrDataParam == '' || typeof cnrDataParam != 'string') {
+      this.#cnrItemData = '';
+      return;
+    };
+
+    this.#cnrItemData = cnrDataParam;
+  };
+
+}; // cnrDataItemClass
+
+/* ************************************************************************* */
+
+export class cnrDataListClass {
+  // PRIVATE
+
+  #cnrDataListID;
+  #cnrDataListCreatedTimeUTC;
+  #cnrDataListType;
+  #cnrDataList = [];
+
+  constructor() {
+    this.#cnrDataListID = self.crypto.randomUUID();
+    const cnrDateTimeVar = new Date();
+    const cnrDateTimeUTCVar = cnrDateTimeVar.toUTCString();
+    this.#cnrDataListCreatedTimeUTC = cnrDateTimeUTCVar;
+    this.#cnrDataListType = "string";
+    this.#cnrDataList = [];
+  };
+
+  // PUBLIC
+
+  // list
+  /**	Returns data list's UUID. */
+  cnrDataListGetID() { return this.#cnrDataListID; };
+
+  /**	Returns data list's created time in UTC format. */
+  cnrDataListGetCreatedTime() { return this.#cnrDataListCreatedTimeUTC; };
+
+  /**	Returns data list's type. */
+  cnrDataListGetType() { return this.#cnrDataListType; };
+
+  /**	Returns data list's items array. */
+  cnrDataListGetList() { return this.#cnrDataList; };
+
+  // items
+  /**	Returns data list's items array length. */
+  cnrDataListGetItemCount() { return this.#cnrDataList.length; };
+
+  /**	Returns item object for index. */
+  cnrDataListGetItemForIndex(cnrItemIndexParam) {
+    if (this.#cnrDataList.length == 0) { return null; };
+    if (cnrItemIndexParam >= this.#cnrDataList.length) { return null; };
+
+    return this.#cnrDataList[cnrItemIndexParam].cnrDataItemGetItem();
+  };
+
+  /**	Returns array of item objects for key. */
+  cnrDataListGetItemsForTag(cnrItemKeyParam) {
+    if (this.#cnrDataList.length == 0) { return null; };
+
+    const cnrItemsArray = [];
+    let cnrCounterVar = 0;
+    const cnrLengthVar = this.#cnrDataList.length;
+    for(cnrCounterVar = 0; cnrCounterVar < cnrLengthVar; cnrCounterVar++) {
+      if (this.#cnrDataList[cnrCounterVar].cnrDataItemGetTag().toString() == cnrItemKeyParam.toString()) {
+        cnrItemsArray.push(this.#cnrDataList[cnrCounterVar].cnrDataItemGetItem());
+      };
+    };
+
+    if (cnrItemsArray.length <= 0) {
+      return null;
+    };
+
+    return cnrItemsArray;
+  };
+
+  /**	Returns item object for UUID. */
+  cnrDataListGetItemForID(cnrItemIDParam) {
+    if (this.#cnrDataList.length == 0) { return null; };
+
+    let cnrCounterVar = 0;
+    const cnrLengthVar = this.#cnrDataList.length;
+    for(cnrCounterVar = 0; cnrCounterVar < cnrLengthVar; cnrCounterVar++) {
+      if (this.#cnrDataList[cnrCounterVar].cnrDataItemGetID().toString() == cnrItemIDParam.toString()) {
+        return this.#cnrDataList[cnrCounterVar].cnrDataItemGetItem();
+      };
+    };
+
+    return null;
+  };
+
+  /**	Returns item's data for key. */
+  cnrDataListGetItemDataForID(cnrItemIDParam) {
+    if (this.#cnrDataList.length == 0) { return ''; };
+
+    return this.cnrDataListGetItemsForTag(cnrItemIDParam).toString();
+  };
+  
+  /**	Adds a data item to the array.
+   * Type is required.
+   * Tag is required but not unique.
+   * Data can be empty 
+   * */
+  cnrDataListAddItem(cnrTypeParam, cnrTagParam, cnrDataParam) {
+    if (cnrTypeParam == null || cnrTypeParam == '') { return; };
+    if (cnrTagParam == null || cnrTagParam == '') { return; };
+
+    let cnrDataVar = cnrDataParam;
+    if (cnrDataParam == null || cnrDataParam == '' || typeof cnrDataParam != 'string') {
+      cnrDataVar = '';
+    };
+
+    const cnrNewItemVar = new cnrDataItemClass(cnrTypeParam, cnrTagParam, cnrDataVar);
+    this.#cnrDataList.push(cnrNewItemVar);
+
     return cnrNewItemVar;
   };
 
 }; // cnrDataListClass
-
-export class cnrDataItemClass {
-  constructor() {
-    this.cnrItemID = "cnrItemID";
-    this.cnrItemCreatedTime = "cnrItemCreatedTime";
-    this.cnrItemType = "cnrItemType";
-    this.cnrItemData = {
-      cnrTitle: 'cnrTitle',
-      cnrStatus: 'x'
-    };
-  };
-}; // cnrDataItemClass
-
-
-function cnrDataRenderList(cnrContainerElementParam, cnrHikeListParam) {
-  cnrHikeListParam.forEach(cnrHikeVar => {
-    cnrContainerElementParam.appendChild(cnrDataRenderItem(cnrHikeVar));
-  });
-};
-
-function cnrDataRenderItem(cnrDataObjectParam) {
-  const cnrHikeObject = {
-    cnrTitle: cnrDataObjectParam.cnrTitle,
-    cnrStatus: cnrDataObjectParam.cnrStatus
-  };
-  const cnrEncodedStringVar = new URLSearchParams(cnrHikeObject).toString();
-  
-  const cnrCardDivVar = document.createElement('div');
-  cnrCardDivVar.classList.add('carddivs');
-  cnrCardDivVar.innerHTML = `
-    <div class="carddivs" title="carddivs">
-      <div class="cardheaderdivs" title="cardheaderdivs">
-        <p class="cardheadertitles">${cnrHikeParam.cnrName}</p>
-        <p class="cardheadertitles">${cnrHikeParam.cnrName}</p>
-        <p class="cardheadertitles">${cnrHikeParam.cnrName}</p>
-      </div>
-    </div>
-`;
-
-  return cnrCardDivVar;
-}; // cnrRenderOneHike
-
-// const cnrDataList = [
-//   {
-//     cnrName: 'Bechler Falls',
-//     cnrImageURL: 'http://byui-cit.github.io/cit261/examples/falls.jpg',
-//     cnrLocation: 'Ashton, Texas',
-//     cnrRating: '3.5',
-//     cnrDifficulty: 'Easy',
-//     cnrDescription:
-//       'Beautiful short hike along the Bechler river to Bechler Falls',
-//     cnrDirections:
-//       'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
-//   }
-// ];
-
-// const cnrDataObject = Object({
-//   cnrDataID: "",
-//   cnrDataCreatedTime: "",
-//   cnrDataType: "",
-//   cnrData: {
-//     cnrName: 'Bechler Falls',
-//     cnrImageURL: 'http://byui-cit.github.io/cit261/examples/falls.jpg',
-//     cnrLocation: 'Ashton, Texas',
-//     cnrRating: '3.5',
-//     cnrDifficulty: 'Easy',
-//     cnrDescription:
-//       'Beautiful short hike along the Bechler river to Bechler Falls',
-//     cnrDirections:
-//       'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.'
-//   }
-// });
--- js/w06/cnrMainModule.js
+++ js/w06/cnrMainModule.js
@@ -5,49 +5,125 @@
 
 
 /* ************************************************************************* */
-
-import * as cnrData from './cnrDataModule.js';
-import * as cnrStorage from './cnrStorageModule.js';
-
 // INITIALIZE
-window.onload = function() {
+
+import * as cnrDataModule from './cnrDataModule.js';
+import * as cnrStorageModule from './cnrStorageModule.js';
+
+const cnrTODOList = new cnrDataModule.cnrDataListClass();
+
+window.onload = function () {
+  cnrTODOList.cnrDataListAddItem('string', 'log', 'window.onload');
+
   // init event handlers
   const cnrAddItemButtonVar = document.getElementById('cnradditembutton');
   if ("ontouchend" in document.documentElement) {
     console.log("Using touchend");
-    cnrAddItemButtonVar.addEventListener('touchend', cnrAddItemClickHandler);
+    cnrAddItemButtonVar.addEventListener('touchend', cnrAddItemButtonHandler);
   }
   else {
     console.log("Using click");
-    cnrAddItemButtonVar.addEventListener('click', cnrAddItemClickHandler);
+    cnrAddItemButtonVar.addEventListener('click', cnrAddItemButtonHandler);
   };
 
+  const cnrDeleteItemButtonsVar = document.querySelectorAll('.cnrdeleteitembuttons');
+  for (const cnrDeleteItemButtonVar of cnrDeleteItemButtonsVar) {
+    if ("ontouchend" in document.documentElement) {
+      console.log("Using touchend");
+      cnrDeleteItemButtonVar.addEventListener('touchend', cnrDeleteItemButtonHandler);
+    }
+    else {
+      console.log("Using click");
+      cnrDeleteItemButtonVar.addEventListener('click', cnrDeleteItemButtonHandler);
+    };
+  };
+
+  // handle onload event
   cnrWindowOnLoadHandler();
 
 };
 
+
+/* ************************************************************************* */
 // EVENT HANDLERS
 function cnrWindowOnLoadHandler(cnrEventParam) {
+  const cnrNewItemVar = cnrTODOList.cnrDataListAddItem('string', 'log', 'cnrWindowOnLoadHandler');
+  console.log("cnrTODOList.cnrDataListGetItemCount = " + cnrTODOList.cnrDataListGetItemCount());
+  console.log("cnrTODOList.cnrDataListGetItemsForKey = " + cnrTODOList.cnrDataListGetItemsForTag('log')[0].cnrDataItemGetData());
+
   // display todo list
-  const cnrTODOList = new cnrData.cnrDataListClass();
-  cnrTODOList.cnrDataDisplayList('jscontainerdiv1');
-  
+
+  cnrDataRenderItem(document.getElementById("cnrjscontainerdiv1"), cnrNewItemVar);
+
 };
 
-function cnrAddItemClickHandler(cnrEventParam) {
+/**	Adds a new item to the list */
+function cnrAddItemButtonHandler(cnrEventParam) {
   cnrEventParam.preventDefault();
   const cnrIDVar = cnrEventParam.target.id;
+  if (cnrIDVar != "cnradditembutton") { return; };
 
-  switch (cnrIDVar) {
-    case "cnradditembutton":
-      cnrStorage.cnrLocalStorageCreate(cnrIDVar, cnrIDVar);
-      break;
-    case "cnrdeleteitembutton":
-      cnrStorage.cnrLocalStorageDeleteKey(cnrIDVar);
-      break;
-  
-    default:
-      break;
-  }
+  // add item to list
+  const cnrToDoTitleVar = document.getElementById('cnradditeminput').value;
+  console.log("cnrToDoTitleVar = " + cnrToDoTitleVar);
+
+  const cnrNewItemVar = cnrTODOList.cnrDataListAddItem('tag', 'todo', cnrToDoTitleVar);
+
+  console.log("cnrDataListGetItemCount = " + cnrTODOList.cnrDataListGetItemCount());
+  console.log("cnrDataListGetItemsForTag = " + cnrTODOList.cnrDataListGetItemsForTag('log')[0].cnrDataItemGetData());
+
+  // display new item
+  cnrDataRenderItem(document.getElementById("cnrjscontainerdiv1"), cnrNewItemVar);
+
+  // update storage
+  // cnrStorageModule.cnrLocalStorageCreate('todo', cnrToDoTitleVar);
+
+};
+
+function cnrDeleteItemButtonHandler(cnrEventParam) {
+  cnrEventParam.preventDefault();
+  if (!cnrEventParam.classList.contains('cnrdeleteitembuttons')) { return; };
+
+  const cnrIDVar = cnrEventParam.target.id;
+  cnrStorageModule.cnrLocalStorageDeleteKey(cnrIDVar);
   
 };
+
+
+/* ************************************************************************* */
+// DISPLAY HANDLERS
+
+/**	Renders the list of data items. */
+function cnrDataRenderList(cnrContainerElementParam, cnrDataListParam) {
+  cnrDataListParam.forEach(cnrDataItemVar => {
+    cnrContainerElementParam.appendChild(cnrDataRenderItem(cnrDataItemVar));
+  });
+};
+
+/**	Renders one data item. */
+function cnrDataRenderItem(cnrContainerElementParam, cnrDataItemParam) {
+  console.log("cnrDataRenderItem > cnrDataItemParam = " + cnrDataItemParam);
+
+  const cnrItemVar = cnrDataItemParam;
+
+  const cnrJSContentDivVar = document.createElement('div');
+  cnrJSContentDivVar.classList.add('cnrjscontentdivs');
+  cnrJSContentDivVar.setAttribute('id', cnrItemVar.cnrDataItemGetID());
+  cnrJSContentDivVar.innerHTML = `
+    <div class="cnrjsstatusdivs">X</div>
+    <div class="cnrjstaskdivs">Task Name Goes Here</div>
+    <div class="cnrjsdeleteitemdivs">X</div>
+  `;
+
+  cnrContainerElementParam.appendChild(cnrJSContentDivVar);
+
+  return cnrJSContentDivVar;
+
+  /*	
+    ${cnrItemVar.cnrDataItemGetData()}
+    ${cnrItemVar.cnrDataItemGetData()}
+    ${cnrItemVar.cnrDataItemGetData()}
+  */
+  
+}; // cnrDataRenderItem
+
