3a6728f27f4cbc0c7909c591b70b247720956e7f
Carlos N Reina <cnreina@gmail.com>
2022-11-12 14:57:02 -0700

    ...

---
+++ cnrCode/w10/js/cnrSecrets.js
@@ -0,0 +1,13 @@
+/*	Carlos N Reina
+  cnreina@gmail.com
+  cnreina.com
+*/
+
+/**	cnrSecrets 
+ * Holds keys, passwords, Etc. 
+*/
+
+/* ************************************************************************* */
+// SECRETS
+
+export const cnrAPI_KEY_MAP = "AIzaSyAzxh-nw2DhH8U8qFunODSK1kAlG1TdAPY";
