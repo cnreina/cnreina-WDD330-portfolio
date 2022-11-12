f82ebd34c0215c23ee5bce9589673df99c78e066
Carlos N Reina <cnreina@gmail.com>
2022-11-12 14:22:29 -0700

    ...

--- cnrCode/w10/js/cnrMapView.js
+++ cnrCode/w10/js/cnrMapView.js
@@ -12,7 +12,27 @@
 /* ************************************************************************* */
 // INITIALIZE
 
+import * as cnrSecrets from '../../../cnrSecret/cnrSecrets.js';
+
 let cnrMap;
+const cnrMAP_API_KEY = cnrSecrets.cnrAPI_KEY_MAP;
+const cnrMAP_API_VERSION = `weekly`;
+const cnrMAP_API_SRC = `https://maps.googleapis.com/maps/api/js?key=${cnrMAP_API_KEY}&callback=cnrInitMap&v=${cnrMAP_API_VERSION}`;
+
+window.onload = function () {
+  // init map
+  const cnrBodyElement = document.getElementsByTagName('body');
+  const cnrMapScriptTag = document.createElement('script');
+  cnrMapScriptTag.src = cnrMAP_API_SRC;
+  cnrBodyElement[0].appendChild(cnrMapScriptTag);
+  window.cnrInitMap = cnrInitMap;
+  
+  console.log('TEST: ', );
+  
+}; // window.onload
+
+/* ************************************************************************* */
+// CONTROL
 
 function cnrInitMap() {
   cnrMap = new google.maps.Map(document.getElementById("map"), {
@@ -20,5 +40,3 @@ function cnrInitMap() {
     zoom: 8,
   });
 };
-
-window.cnrInitMap = cnrInitMap;
